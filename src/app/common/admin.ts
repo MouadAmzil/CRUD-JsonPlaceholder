dbdea9b3dd6ec2a631e5a669aea75f4745ee85b4
MouadAmzil <mouadmouad2026@gmail.com>
2022-03-16 16:32:44 +0100

    Add check is admin in login

--- src/app/common/admin.ts
+++ src/app/common/admin.ts
@@ -1,8 +1,9 @@
 import { User } from '../models/user.interface';
 
 export class Admin {
-  public static ListOfAdmin: User[] = [
+  public static ListOFAdmin: User[] = [
     { id: 1, email: 'admin@gmail.com', password: 'password', isAdmin: true },
     { id: 2, email: 'admin1@gmail.com', password: 'password', isAdmin: true },
   ];
+  public static ThisUSerISAdmin = false;
 }
--- src/app/services/local-storge.service.ts
+++ src/app/services/local-storge.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { User } from '../models/user.interface';
+import { Admin } from '../common/Admin';
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorgeService {
   constructor() {}
+  Task: User = {
+    id: Math.floor(Math.random() * 1000) + 1,
+    email: '',
+    password: '',
+    isAdmin: false,
+  };
 
   SETLoaclStorageLogin(obj: object) {
+    this.checkISAdmin(obj['email'], obj['password']);
     localStorage.setItem('userLogin', JSON.stringify(obj));
   }
   GEtLoaclStorageLogin(): object {
@@ -22,6 +30,18 @@ export class LocalStorgeService {
     return true;
   }
   DELETELoaclStorageLogin() {
+    Admin.ThisUSerISAdmin = false;
     localStorage.clear();
+  }
+
+  checkISAdmin(email: string, password: string) {
+    for (let i = 0; i < Admin.ListOFAdmin.length; i++) {
+      if (
+        Admin.ListOFAdmin[i].email === email &&
+        Admin.ListOFAdmin[i].password === password
+      ) {
+        Admin.ThisUSerISAdmin = true;
+      }
+    }
   }
 }

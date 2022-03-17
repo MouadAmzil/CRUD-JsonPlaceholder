c6ce9c8cfaad63287567b076059f593915499af9
MouadAmzil <mouadmouad2026@gmail.com>
2022-03-17 11:43:09 +0100

    Add Some Event to edit Post

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -2,6 +2,7 @@ import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../services/post/post.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -9,28 +10,48 @@ import { PostService } from '../services/post/post.service';
   styleUrls: ['./edit.component.css'],
 })
 export class EditComponent implements OnInit {
-  postSelected: any | undefined = { userId: 0, id: 0, title: '', body: '' };
+  postSelected: any | undefined = {
+    userId: 0,
+    id: 0,
+    title: '',
+    body: '',
+  };
+  post: FormGroup;
   constructor(
-    private PostSV: PostService,
+    private _PostSV: PostService,
     private _route: ActivatedRoute,
-    private _location: Location
+    private _location: Location,
+    private _fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
+    this.setForm();
     this.GetPost();
   }
   GetPost() {
-    this.PostSV.GetPostById(
-      String(this._route.snapshot.paramMap.get('id'))
-    ).subscribe({
-      next: (res) => {
-        this.postSelected = res;
-        console.log(res);
-      },
-      error: (err) => console.log(err),
-    });
+    this._PostSV
+      .GetPostById(String(this._route.snapshot.paramMap.get('id')))
+      .subscribe({
+        next: (res) => {
+          this.postSelected = res;
+          this.setForm();
+        },
+        error: (err) => console.log(err),
+      });
+  }
+  deletePost() {
+    console.log('first');
+  }
+  updatePost() {
+    console.log('first Post');
   }
   GoBack() {
     this._location.back();
+  }
+  setForm() {
+    this.post = this._fb.group({
+      title: [this.postSelected['title'], [Validators.required]],
+      body: [this.postSelected['body'], [Validators.required]],
+    });
   }
 }
